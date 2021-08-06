3aa89b999551f4d7ce737764dd2f4b6c3314ba24
Verena Yiu <verenayiu@berkeley.edu>
2021-08-05 17:43:32 -0700

    added urls

--- draw/urls.py
+++ draw/urls.py
@@ -6,5 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-
+    path('home/', views.home, name='home'),
+    path('create/', views.create, name='create'),
+    path('define/', views.define, name='define'),
+    path('sentence/', views.sentence, name='sentence'),
+    path('saved/', views.saved, name='saved'),
 ]
