2733da618fc49934732a2468d801a62f7db0dc22
Ting Qi <tina0801@berkeley.edu>
2021-08-05 17:45:34 -0700

    veiws change

--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,21 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def home(request):
+    return render(request, 'draw/home.html')
+
+def create(request):
+    return render(request, 'draw/create.html')
+
+def define(request):
+    return render(request, 'draw/define.html')
+
+def sentence(request):
+    return render(request, 'draw/sentence.html')
+
+def saved(request):
+    return render(request, 'draw/saved.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
